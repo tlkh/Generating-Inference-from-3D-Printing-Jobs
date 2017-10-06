6c50c028d09b6220becde070bde42d99df70efff
Timothy Liu <tlkh@users.noreply.github.com>
2017-10-06 23:34:30 +0800

    Plot the Data
    Some correlations are visible.

--- clustering.py
+++ clustering.py
@@ -1,9 +1,54 @@
 from preprocess import *
 import csv
 
-with open('merged.csv', 'r') as f:
+# to combine csv files: $ cat *.csv > merged.csv
+
+with open('/Users/timothy/Desktop/data/merged.csv', 'r') as f:
       reader = csv.reader(f)
       data = list(reader)
 
 matrix = obtain_data_matrix(data)
-print(matrix)
+samples = len(matrix)
+print("Number of samples: " + str(samples))
+#print(matrix[0]) #[[2680 1 0 0 0 0 0 0 4.9481 72 5 0]]
+
+filament = matrix[:,[8]]
+time = matrix[:,[9]]
+satisfaction = matrix[:,[10]]
+result = matrix[:,[11]]
+
+cat_gift = matrix[:,[6]]
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+#3D Plot
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter([filament], [time], [satisfaction])'''
+
+plt.figure(1)
+plt.subplot(212)
+plt.xlabel('Print Time (mins)')
+plt.ylabel('Filament (m)')
+plt.title('Filament Use')
+plt.scatter([filament], [time])
+
+plt.subplot(221)
+plt.xlabel('Print Time (mins)')
+plt.title('Rating')
+plt.scatter([time], [satisfaction])
+
+plt.subplot(222)
+plt.xlabel('Print Time (mins)')
+plt.title('Success / Fail / NIL')
+plt.scatter([time], [result])
+
+plt.tight_layout()
+plt.show()
+
+
--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ def generate_purpose_vector(raw_data):
     purpose[2]=raw_data.count("Test Ergonomics")
     purpose[3]=raw_data.count("Wearout")
     purpose[4]=raw_data.count("Integration")
-    purpose[5]=raw_data.count("Ornamental / Design" or "Ornamental / Gift")
+    purpose[5]=raw_data.count("Ornamental / Design" or "Ornamental / Gift" or "Design (Look)")
     purpose[6]=raw_data.count("Others:")
     return purpose
 
@@ -24,25 +24,42 @@ def generate_filament_use(raw_data):
 
 def generate_time_taken(raw_data):
     try:
-        time = float(raw_data)
+        time = int(raw_data)
     except:
         time = str(raw_data)
-        words_to_remove = [" ","hour","minute","min","hr","h","s","m"]
-        for word in words_to_remove:
-            time = time.replace(word,"")
-        time = float(time)
+        time = time.replace(" ","")
+        time = time.replace("s","")
+        if "h" in time:
+            words_to_remove = ["our","m","in","ute","r"]
+            for word in words_to_remove:
+                time = time.replace(word,"")
+            time_array = time.split("h")
+            hour = int(float(time_array[0]))
+            if time_array[1] == "":
+                minutes = 0
+            else:
+                minutes = int(time_array[1])
+            time = int(hour*60 + minutes)
+        elif "m" in time:
+            words_to_remove = ["in","ute"]
+            for word in words_to_remove:
+                time = time.replace(word,"")
+            time_array = time.split("m")
+            time = int(time_array[0])
+        else:
+            time = int(float(time)*60)
     return time
 
 def generate_feedback(raw_data):
     feedback = 0 #uncertain
     if (raw_data.count("fail" or "Fail") > 0):
         feedback = -1 #fail
-    elif (raw_data.count("good" or "well") > 0):
+    if (raw_data.count("Good" or "good" or "well" or "ok") > 0):
         feedback = 1 #success
     return feedback
 
 def obtain_data_matrix(data):
-    matrix = np.matrix([0,(0,0,0,0,0,0,0),0,0,0,0], dtype=object)
+    matrix = np.matrix([0,0,0,0,0,0,0,0,0,0,0,0], dtype=object)
 
     for raw in data:
         stu_id = int(raw[2]) - 1000000
@@ -58,7 +75,7 @@ def obtain_data_matrix(data):
         satisfaction = int(raw[8])
         feedback = generate_feedback(raw[9])
 
-        point = np.array([stu_id, purpose, filament, time, satisfaction, feedback], dtype=object)
+        point = np.array([stu_id, purpose[0], purpose[1], purpose[2], purpose[3], purpose[4], purpose[5], purpose[6], filament, time, satisfaction, feedback], dtype=object)
         matrix = np.vstack([matrix, point])
 
     matrix = np.delete(matrix, (0), axis=0)
